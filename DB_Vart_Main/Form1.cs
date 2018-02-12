6bce5ec38cc5a19aa2a08c719fb2e8ecfdf370c1
Ruma00 <hackgen0@gmail.com>
2018-02-12 15:59:22 +0700

    TextBoxes processing

--- DB_Vart_Main/Form1.cs
+++ DB_Vart_Main/Form1.cs
@@ -19,6 +19,17 @@ namespace DB_Vart_Main
             SqlConnection sqlConnection = new SqlConnection();
             sqlConnection.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Base;Integrated Security=true";
             sqlConnection.Open();
+
+            textBoxSD.Enter += new EventHandler(textBoxSD_Enter);
+            textBoxSD.Leave += new EventHandler(textBoxSD_Leave);
+            textBoxSA.Enter += new EventHandler(textBoxSA_Enter);
+            textBoxSA.Leave += new EventHandler(textBoxSA_Leave);
+            textBoxPayCH.Enter += new EventHandler(textBoxPayCH_Enter);
+            textBoxPayCH.Leave += new EventHandler(textBoxPayCH_Leave);
+            textBoxAddP.Enter += new EventHandler(textBoxAddP_Enter);
+            textBoxAddP.Leave += new EventHandler(textBoxAddP_Leave);
+            textBoxDel.Enter += new EventHandler(textBoxDel_Enter);
+            textBoxDel.Leave += new EventHandler(textBoxDel_Leave);
         }
 
         string[] sqlExpressions = new string[] { "SELECT * FROM Main", "UPDATE Main SET ", "INSERT INTO Main VALUES ", "WHERE Contract_num=" };
@@ -26,6 +37,97 @@ namespace DB_Vart_Main
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        //--------------------------textBoxes-------------------------------------------------
+        private void textBoxSD_Enter(object sender, EventArgs e)
+        {
+            if (textBoxSD.Text == "Введите № договора")
+            {
+                textBoxSD.Clear();
+                textBoxSD.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxSD_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSD.Text))
+            {
+                textBoxSD.Text = "Введите № договора";
+                textBoxSD.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxSA_Enter(object sender, EventArgs e)
+        {
+            if (textBoxSA.Text == "Введите адрес")
+            {
+                textBoxSA.Clear();
+                textBoxSA.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxSA_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSA.Text))
+            {
+                textBoxSA.Text = "Введите адрес";
+                textBoxSA.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxPayCH_Enter(object sender, EventArgs e)
+        {
+            if (textBoxSD.Text == "Смена аб. платы")
+            {
+                textBoxPayCH.Clear();
+                textBoxPayCH.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxPayCH_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSD.Text))
+            {
+                textBoxPayCH.Text = "Смена аб. платы";
+                textBoxPayCH.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxAddP_Enter(object sender, EventArgs e)
+        {
+            if (textBoxSD.Text == "Введите адрес или № договора")
+            {
+                textBoxAddP.Clear();
+                textBoxAddP.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxAddP_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSD.Text))
+            {
+                textBoxAddP.Text = "Введите адрес или № договора";
+                textBoxAddP.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxDel_Enter(object sender, EventArgs e)
+        {
+            if (textBoxDel.Text == "Введите адрес или № договора")
+            {
+                textBoxDel.Clear();
+                textBoxDel.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxDel_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSD.Text))
+            {
+                textBoxDel.Text = "Введите адрес или № договора";
+                textBoxDel.ForeColor = Color.Gray;
+            }
         }
     }
 }
