410b61c6827e4a80e7d893c90cbf5365e6e4a016
Ruma00 <hackgen0@gmail.com>
2018-02-12 15:41:09 +0700

    Connecting to db

--- DB_Vart_Main/Form1.cs
+++ DB_Vart_Main/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,12 @@ namespace DB_Vart_Main
         public Form1()
         {
             InitializeComponent();
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Base;Integrated Security=true";
+            sqlConnection.Open();
         }
+
+        string[] sqlExpressions = new string[] { "SELECT * FROM Main", "UPDATE Main SET ", "INSERT INTO Main VALUES ", "WHERE Contract_num=" };
 
         private void button1_Click(object sender, EventArgs e)
         {
